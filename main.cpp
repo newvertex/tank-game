d61e8a91db32f064b929d84f0dc8c46acf2cbad4
newvertex <mojtaba.r127@gmail.com>
2021-10-25 16:57:22 +0330

    add basic raylib window

--- main.cpp
+++ main.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
+#include <raylib.h>
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+constexpr auto SCREEN_WIDTH = 640;
+constexpr auto SCREEN_HEIGHT = 480;
+constexpr auto TITLE = "Rusty Tank";
+constexpr auto TARGET_FPS = 60;
+
+int main(int argc, char* argv[])
+{
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, TITLE);
+    SetTargetFPS(TARGET_FPS);
+
+    bool isGameRunning = true;
+
+    while (isGameRunning)
+    {
+        // Update
+        isGameRunning = !WindowShouldClose();
+
+        // Render
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
 }
